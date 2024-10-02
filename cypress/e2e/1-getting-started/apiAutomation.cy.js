c603387bcf82e63b0c059f7d396dc7b0a57b1c37
informa1980 <you@example.com>
2024-10-02 17:57:20 +0530

    Commit the updated .yml file

---
+++ cypress/e2e/1-getting-started/apiAutomation.cy.js
@@ -0,0 +1,34 @@
+/// <reference types="cypress" />
+describe("API QAAutomationLabs.com", { testIsolation: false }, () => {
+    it("GET API testing Using Cypress API Plugin", () => {
+      cy.request("GET", "https://reqres.in/api/users?page=2").should(
+        (response) => {
+          expect(response.status).to.eq(200);
+        }
+      );
+    });
+  
+    it("POST API testing Using Cypress API Plugin", () => {
+      cy.request("POST", "https://reqres.in/api/users", {
+        name: "morpheus",
+        job: "leader",
+      }).should((response) => {
+        expect(response.status).to.eq(201);
+      });
+    });
+  
+    it("PUT API testing Using Flip Plugin", () => {
+      cy.request("PUT", "https://reqres.in/api/users/2", {
+        name: "morpheus",
+        job: "zion resident",
+      }).should((response) => {
+        expect(response.status).to.eq(200);
+      });
+    });
+  
+    it("DELETE API testing Using Cypress API Plugin", () => {
+      cy.request("DELETE", "https://reqres.in/api/users/2").should((response) => {
+        expect(response.status).to.eq(204);
+      });
+    });
+  });
