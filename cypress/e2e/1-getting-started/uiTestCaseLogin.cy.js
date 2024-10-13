71bc937d9aeb67be6e1dd2981f07eeb8ead807be
informa1980 <you@example.com>
2024-10-13 19:24:21 +0530

    update test case for -ve testing

--- cypress/e2e/1-getting-started/uiTestCaseLogin.cy.js
+++ cypress/e2e/1-getting-started/uiTestCaseLogin.cy.js
@@ -9,6 +9,6 @@ describe("Login to Saucedemo", () => {
     cy.get("#password").type(password);
     cy.get("#login-button").click();
     cy.get("#react-burger-menu-btn").click();
-    cy.get("#logout_sidebar_link").click();
+    cy.get("#logout_sidebar_links").click();
   });
 });
