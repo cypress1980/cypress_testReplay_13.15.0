02471a6a4b1447a8cb4f1c7542fbd8ab694c0f79
informa1980 <you@example.com>
2024-10-13 19:47:35 +0530

    added cy.log and console.log

--- cypress/e2e/1-getting-started/uiTestCaseLogin.cy.js
+++ cypress/e2e/1-getting-started/uiTestCaseLogin.cy.js
@@ -5,12 +5,14 @@ const password = "secret_sauce";
 describe("Login to Saucedemo", () => {
   it("should log in with valid credentials", () => {
     cy.visit("https://www.saucedemo.com/");
+    console.log('User name is --> ',username)
     cy.get("#user-name").type(username);
+    console.log('Password is --> ',password)
     cy.get("#password").type(password);
     cy.get("#login-button").click();
     cy.log('Clicked on Login Button')
     cy.get("#react-burger-menu-btn").click();
-    cy.log('Clicked on Burger Menu Button')
+    cy.log('Clicked on hamburger button')
     cy.get("#logout_sidebar_links").click();
   });
 });
