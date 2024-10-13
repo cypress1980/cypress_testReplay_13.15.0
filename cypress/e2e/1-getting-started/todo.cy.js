f0d199680e3a28eea53179f32ab6671fffd3ce1d
informa1980 <you@example.com>
2024-10-13 19:09:16 +0530

    .yml updaated with 2 machine

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -14,7 +14,7 @@ describe('example to-do app', () => {
     const newItem = 'Feed the cat'
     cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
     cy.get('.todo-list li')
-      .should('have.length', 4)
+      .should('have.length', 3)
       .last()
       .should('have.text', newItem)
   })
